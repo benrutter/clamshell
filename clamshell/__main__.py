4da60afb4320fcc0537d714d67c63e95bc505ce4
Ben <ben.rutter@flexitricity.com>
2023-01-09 11:41:24 +0000

    "executable issue fix"

--- clamshell/__main__.py
+++ clamshell/__main__.py
@@ -20,44 +20,47 @@ from .shell_utils import (
     splitter,
 )
 
-if __name__ == "__main__":
 
-    clamshell = ClamShell(
-        super_commands=["files", "exit", "clear"],
-        aliases={"_": "_"},
-        shell_globals=globals(),
-        shell_locals=locals(),
+clamshell = ClamShell(
+    super_commands=["files", "exit", "clear"],
+    aliases={"_": "_"},
+    shell_globals=globals(),
+    shell_locals=locals(),
+)
+
+try:
+    rc_path, rc_file = clamshell.rc_file().rsplit(splitter, 1)
+    sys.path.append(rc_path)
+    from clamrc import *
+
+    if "super_commands" in locals():
+        clamshell.super_commands += super_commands
+        del super_commands
+    if "aliases" in locals():
+        clamshell.aliases.update(aliases)
+        del aliases
+    if "get_prompt" in locals():
+        clamshell.get_prompt = get_prompt
+        del get_prompt
+    if "get_continuation_prompt" in locals():
+        clamshell.get_continuation_prompt = get_continuation_prompt
+        del get_continuation_prompt
+    del splitter
+    del sys
+    del rc_path
+    del rc_file
+except:
+    print(
+        "[red]Error loading/running clamrc!\n"
+        "(likely error in .config/clamrc.py script)[/red]"
     )
 
-    try:
-        rc_path, rc_file = clamshell.rc_file().rsplit(splitter, 1)
-        sys.path.append(rc_path)
-        from clamrc import *
-
-        if "super_commands" in locals():
-            clamshell.super_commands += super_commands
-            del super_commands
-        if "aliases" in locals():
-            clamshell.aliases.update(aliases)
-            del aliases
-        if "get_prompt" in locals():
-            clamshell.get_prompt = get_prompt
-            del get_prompt
-        if "get_continuation_prompt" in locals():
-            clamshell.get_continuation_prompt = get_continuation_prompt
-            del get_continuation_prompt
-        del splitter
-        del sys
-        del rc_path
-        del rc_file
-    except:
-        print(
-            "[red]Error loading/running clamrc!\n"
-            "(likely error in .config/clamrc.py script)[/red]"
-        )
-
+def run_clam():
     while True:
         try:
             clamshell.repl()
         except KeyboardInterrupt:
             clamshell.print_output("")
+
+if __name__ == '__main__':
+    run_clam()
